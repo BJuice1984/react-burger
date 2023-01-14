f16dd113561776ae698a2a7de80de4117cbd05a9
BJuice <beetle_juice84@mail.ru>
2023-01-14 19:41:45 +0300

    feat/Создать компонент Header. Создать структуру папок приложения

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import Header from '../Header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="page">
+      <div className="page__container">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
+
+export default function Header() {
+  return(
+    <header className="header">
+      <Logo />
+    </header>
+
+  )
+}
