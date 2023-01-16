29c3ae1c2beee58effbc77160796a67555ce273d
BJuice <beetle_juice84@mail.ru>
2023-01-16 08:11:26 +0300

    feat/Создать компонент BurgerIngredientsCard. Вёрстка + CSS + PropTypes

--- src/components/BurgerIngredients/BurgerIngredients.js
+++ src/components/BurgerIngredients/BurgerIngredients.js
@@ -43,22 +43,6 @@ export default function BurgerIngredients({ bunIngridients, sauceIngridients, ma
   )
 }
 
-// BurgerIngredients.propTypes = {
-//   initialIngridients: PropTypes.shape({
-//     id: PropTypes.number.isRequired,
-//     name: PropTypes.string.isRequired,
-//     type: PropTypes.string.isRequired,
-//     proteins: PropTypes.number.isRequired,
-//     fat: PropTypes.number.isRequired,
-//     carbohydrates: PropTypes.number.isRequired,
-//     calories: PropTypes.number.isRequired,
-//     price: PropTypes.number.isRequired,
-//     image: PropTypes.string.isRequired,
-//     image_mobile: PropTypes.string.isRequired,
-//     image_large: PropTypes.string.isRequired,
-//     __v: PropTypes.number
-//   }),
-// }
 BurgerIngredients.propTypes = {
   bunIngridients: PropTypes.array.isRequired,
   sauceIngridients: PropTypes.array.isRequired,
--- src/components/BurgerIngredientsCard/BurgerIngredientsCard.js
+++ src/components/BurgerIngredientsCard/BurgerIngredientsCard.js
@@ -1,5 +1,6 @@
+import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './burgerIngredientsCard.module.css';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 export default function BurgerIngredientsCard({ ingridient }) {
   return(
@@ -8,7 +9,29 @@ export default function BurgerIngredientsCard({ ingridient }) {
         src={ingridient.image}
         alt="Картинка. Вид ингридиента">
       </img>
-
+      <p className={ styles.price }>
+        {ingridient.price}<CurrencyIcon type="primary"/>
+      </p>
+      <span className={ styles.name }>
+        {ingridient.name}
+      </span>
     </article>
   )
+}
+
+BurgerIngredientsCard.propTypes = {
+  ingridient: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    proteins: PropTypes.number.isRequired,
+    fat: PropTypes.number.isRequired,
+    carbohydrates: PropTypes.number.isRequired,
+    calories: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    image_mobile: PropTypes.string.isRequired,
+    image_large: PropTypes.string.isRequired,
+    __v: PropTypes.number
+  }),
 }
