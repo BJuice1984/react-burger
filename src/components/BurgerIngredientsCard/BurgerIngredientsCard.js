c1ca33f1454065be5eda3a50d276804c3098f1a7
BJuice <beetle_juice84@mail.ru>
2023-01-17 09:02:53 +0300

    feat/добавить компонент Counter в BurgerIngridientsCard.js

--- src/components/BurgerIngredientsCard/BurgerIngredientsCard.js
+++ src/components/BurgerIngredientsCard/BurgerIngredientsCard.js
@@ -1,10 +1,11 @@
-import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './burgerIngredientsCard.module.css';
 import PropTypes from 'prop-types';
 
 export default function BurgerIngredientsCard({ ingridient }) {
   return(
     <article className={ styles.element }>
+      <Counter count={1} size="default" extraClass="m-1" />
       <img className={ styles.pic }
         src={ingridient.image}
         alt="Картинка. Вид ингридиента">
