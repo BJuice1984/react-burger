e2ad90dd53f15e2347291613a7ab69f3bd2709de
BJuice <beetle_juice84@mail.ru>
2023-01-22 13:12:46 +0300

    refr/Вынести в React.Fragment элементы конструктора в BurgerConstructor.js

--- src/components/BurgerConstructor/BurgerConstructor.js
+++ src/components/BurgerConstructor/BurgerConstructor.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { ConstructorElement, CurrencyIcon, Button, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './burgerConstructor.module.css';
 import ModalOrder from "../ModalOrder/ModalOrder";
@@ -14,112 +14,101 @@ export default function BurgerConstructor({ bunIngridients, sauceIngridients, ma
   return(
     <section className={`${ styles.burgerConstructor } pt-25`}>
       <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }} className={ styles.burgerConstructorContainer }>
-        {bunIngridients.length !== 0 && <ConstructorElement
+        {bunIngridients.length && sauceIngridients.length && mainIngridients.length && 
+        <Fragment>
+          <ConstructorElement
           extraClass="ml-8"
           type="top"
           isLocked={true}
           text={`${bunIngridients[0].name}(верх)`}
           price={200}
-          thumbnail={bunIngridients[0].image}
-        />}
-        <div>
-        <DragIcon type="primary"/>
-        {mainIngridients.length && 
+          thumbnail={bunIngridients[0].image} />
+          <div>
+            <DragIcon type="primary"/>
+              <ConstructorElement
+              text={`${mainIngridients[0].name}`}
+              price={50}
+              thumbnail={mainIngridients[0].image} />
+          </div>
+          <div>
+            <DragIcon type="primary"/>
+              <ConstructorElement
+              text={`${sauceIngridients[0].name}`}
+              price={50}
+              thumbnail={sauceIngridients[0].image} />
+          </div>
+          <div>
+            <DragIcon type="primary"/>
+              <ConstructorElement
+              text={`${sauceIngridients[0].name}`}
+              price={50}
+              thumbnail={sauceIngridients[0].image} />
+          </div>
+          <div>
+            <DragIcon type="primary"/>
+              <ConstructorElement
+              text={`${sauceIngridients[0].name}`}
+              price={50}
+              thumbnail={sauceIngridients[0].image} />
+          </div>
+          <div>
+            <DragIcon type="primary"/>
+              <ConstructorElement
+                text={`${sauceIngridients[0].name}`}
+                price={50}
+                thumbnail={sauceIngridients[0].image} />
+          </div>
+          <div>
+            <DragIcon type="primary"/>
+              <ConstructorElement
+              text={`${sauceIngridients[0].name}`}
+              price={50}
+              thumbnail={sauceIngridients[0].image} />
+          </div>
+          <div>
+            <DragIcon type="primary"/>
+              <ConstructorElement
+              text={`${sauceIngridients[0].name}`}
+              price={50}
+              thumbnail={sauceIngridients[0].image} />
+          </div>
+          <div>
+            <DragIcon type="primary"/>
+              <ConstructorElement
+              text={`${sauceIngridients[0].name}`}
+              price={50}
+              thumbnail={sauceIngridients[0].image} />
+          </div>
+          <div>
+            <DragIcon type="primary"/>
+              <ConstructorElement
+              text={`${sauceIngridients[0].name}`}
+              price={50}
+              thumbnail={sauceIngridients[0].image} />
+          </div>
+          <div>
+            <DragIcon type="primary"/>
+              <ConstructorElement
+              text={`${sauceIngridients[0].name}`}
+              price={50}
+              thumbnail={sauceIngridients[0].image} />
+          </div>
+          <div>
+            <DragIcon type="primary"/>
+              <ConstructorElement
+              text={`${sauceIngridients[0].name}`}
+              price={50}
+              thumbnail={sauceIngridients[0].image} />
+          </div>
           <ConstructorElement
-          text={`${mainIngridients[0].name}`}
-          price={50}
-          thumbnail={mainIngridients[0].image}
-        />}
-        </div>
-        <div>
-        <DragIcon type="primary"/>
-        {sauceIngridients.length && <ConstructorElement
-          text={`${sauceIngridients[0].name}`}
-          price={50}
-          thumbnail={sauceIngridients[0].image}
-        />}
-        </div>
-        <div>
-        <DragIcon type="primary"/>
-        {sauceIngridients.length && <ConstructorElement
-          text={`${sauceIngridients[0].name}`}
-          price={50}
-          thumbnail={sauceIngridients[0].image}
-        />}
-        </div>
-        <div>
-        <DragIcon type="primary"/>
-        {sauceIngridients.length && <ConstructorElement
-          text={`${sauceIngridients[0].name}`}
-          price={50}
-          thumbnail={sauceIngridients[0].image}
-        />}
-        </div>
-        <div>
-        <DragIcon type="primary"/>
-        {sauceIngridients.length && <ConstructorElement
-          text={`${sauceIngridients[0].name}`}
-          price={50}
-          thumbnail={sauceIngridients[0].image}
-        />}
-        </div>
-        <div>
-        <DragIcon type="primary"/>
-        {sauceIngridients.length && <ConstructorElement
-          text={`${sauceIngridients[0].name}`}
-          price={50}
-          thumbnail={sauceIngridients[0].image}
-        />}
-        </div>
-        <div>
-        <DragIcon type="primary"/>
-        {sauceIngridients.length && <ConstructorElement
-          text={`${sauceIngridients[0].name}`}
-          price={50}
-          thumbn
-          ail={sauceIngridients[0].image}
-        />}
-        </div>
-        <div>
-        <DragIcon type="primary"/>
-        {sauceIngridients.length && <ConstructorElement
-          text={`${sauceIngridients[0].name}`}
-          price={50}
-          thumbnail={sauceIngridients[0].image}
-        />}
-        </div>
-        <div>
-        <DragIcon type="primary"/>
-        {sauceIngridients.length && <ConstructorElement
-          text={`${sauceIngridients[0].name}`}
-          price={50}
-          thumbnail={sauceIngridients[0].image}
-        />}
-        </div>
-        <div>
-        <DragIcon type="primary"/>
-        {sauceIngridients.length && <ConstructorElement
-          text={`${sauceIngridients[0].name}`}
-          price={50}
-          thumbnail={sauceIngridients[0].image}
-        />}
-        </div>
-        <div>
-        <DragIcon type="primary"/>
-        {sauceIngridients.length && <ConstructorElement
-          text={`${sauceIngridients[0].name}`}
-          price={50}
-          thumbnail={sauceIngridients[0].image}
-        />}
-        </div>
-        {bunIngridients.length && <ConstructorElement
           extraClass="ml-8"
           type="bottom"
           isLocked={true}
           text={`${bunIngridients[0].name}(низ)`}
           price={200}
-          thumbnail={bunIngridients[0].image}
-        />}
+          thumbnail={bunIngridients[0].image}/>
+        </Fragment>}
+        
       </div>
       <div className={`${ styles.burgerContainer } pt-10`}>
         <span className={`${ styles.burgerElementPrice } text text_type_digits-medium pr-10`}>
