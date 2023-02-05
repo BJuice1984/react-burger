51a9f8e51eecf9b91ae90138ca4ff649d528698d
BJuice <beetle_juice84@mail.ru>
2023-02-05 09:22:48 +0300

    feat/use the store from the ingridientsReducer.js in app/BurgerIngridients

--- src/components/BurgerIngredients/BurgerIngredients.js
+++ src/components/BurgerIngredients/BurgerIngredients.js
@@ -1,5 +1,5 @@
 import styles from './burgerIngridients.module.css';
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import BurgerIngredientsCard from '../BurgerIngredientsCard/BurgerIngredientsCard';
@@ -8,18 +8,11 @@ import { func } from 'prop-types';
 import { burgerIngredientArrayType } from '../../utils/prop-types';
 import { getItems } from '../../services/actions/initialIngridients';
 
-export default function BurgerIngredients({  bunIngridients, sauceIngridients, mainIngridients, handleOpenModal }) {
-
-  // const [bunIngridients, setBunIngridients] = useState([]);
+export default function BurgerIngredients({ handleOpenModal }) {
 
   const groupedIngridients = groupBy(useSelector(state => state.initialIngridients.items), "type");
-  const itemsRequest = useSelector(state => state.initialIngridients.itemsRequest);
+  // const itemsRequest = useSelector(state => state.initialIngridients.itemsRequest);
   const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   setBunIngridients(groupedIngridients.bun);
-  // }, [groupedIngridients.bun])
-  
 
   useEffect(() => {
     dispatch(getItems())
@@ -33,15 +26,12 @@ export default function BurgerIngredients({  bunIngridients, sauceIngridients, m
       return { ...acc, [key]: [...curGroup, obj] };
     }, {});
   }
-  console.log(Object.keys(groupedIngridients).length === 0);
-  // console.log(groupedIngridients.bun);
-  // console.log(bunIngridients)
 
   return(
     <section className={ styles.ingridients }>
       <h2 className={`${ styles.header } text text_type_main-large pt-10`}>Соберите бургер</h2>
       <BurgerTab />
-      { Object.keys(groupedIngridients).length !== 0 && itemsRequest &&
+      { groupedIngridients.bun && groupedIngridients.sauce && groupedIngridients.main &&
         <Fragment>
           <ul className={ styles.ingridientsListContainer }>
             <li className={`${ styles.ingridientsListHeader } text text_type_main-medium`}>
