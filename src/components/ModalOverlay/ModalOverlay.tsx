36d1db930e40f3f62b7fa6c078a7db3d6b095e34
BJuice <beetle_juice84@mail.ru>
2023-05-25 21:28:23 +0300

    feat/add TS in the Modal, ModalOverlay

--- src/components/ModalOverlay/ModalOverlay.tsx
+++ src/components/ModalOverlay/ModalOverlay.tsx
@@ -1,8 +1,14 @@
+import { FC } from 'react';
 import styles from './modalOverlay.module.css';
 
-export default function ModalOverlay(isModalOpen: boolean) {
+type ModalOverlayType = {
+  isModalOpen: boolean
+}
 
+const ModalOverlay: FC<ModalOverlayType> = (isModalOpen) => {
   return(
       <div className={`${ styles.modal } ${isModalOpen ? styles.modalOpened : ''}`}></div>
   )
-}
+};
+
+export default ModalOverlay;
